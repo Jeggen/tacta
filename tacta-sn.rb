a211bdb9348e10d973e46307de4e85be5050b3fb
YOUR NAME <YOUR@EMAIL.com>
2016-06-09 18:07:00 +0530

    links contacts with index

--- tacta-sn.rb
+++ tacta-sn.rb
@@ -1,3 +1,20 @@
+# require 'sinatra'
+#
+# set :port, 4567
+#
+# get '/' do
+#    "<h1>Tacta Contact Manager</h1>"
+# end
+#
+# require './contacts_file'
+#
+# get '/contacts/:i' do
+#    @i = params[:i].to_i
+#    contacts = read_contacts
+#    @contact = contacts[@i]
+#    erb :'contacts/show'
+# end
+
 require 'sinatra'
 
 set :port, 4567
@@ -6,7 +23,13 @@ get '/' do
    "<h1>Tacta Contact Manager</h1>"
 end
 
+
 require './contacts_file'
+
+get '/contacts' do
+   @contacts = read_contacts
+   erb :'contacts/index'
+end
 
 get '/contacts/:i' do
    @i = params[:i].to_i
